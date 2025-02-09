cb12331e1842d4d3a21ddcbcfb51a0b53d0703f2
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-10 00:18:25 +0600

    revalidate data fethings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-const HomePage = () => {
+import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const blogs = await res.json();
+  console.log(blogs);
   return (
     <div className="my-10">
-      <h1 className="text-4xl text-center">Latest Blogs</h1>
+      <LatestBlogs blogs={blogs} />
     </div>
   );
 };
