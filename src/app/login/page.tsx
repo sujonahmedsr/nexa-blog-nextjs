05a188b25bbca55cf99c59e5768173a81fd0a6b7
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-11 21:41:54 +0600

    google login okay

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -101,7 +101,9 @@ const LoginPage = () => {
 
           {/* Social Login Buttons */}
           <div className="flex justify-center gap-4 mt-4">
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
+            <button onClick={()=>signIn("google", {
+              callbackUrl: '/'
+            })} className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={30}
